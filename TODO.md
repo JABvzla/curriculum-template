ff63bd67e9962d73d882f9abeb9629aa890bd852
Jose Bonito <joseantoniobonito@hotmail.com>
2018-04-21 08:48:04 -0400

    add preloader

---
+++ TODO.md
@@ -0,0 +1,24 @@
+##List of `To Do`!
+
+####NavBar
+    -Reponsive hide.
+####Home
+    -Take picture.
+    -Center TextSection.
+    -Set button shadow.
+####Profile
+    -Copy from about.
+####TimeLine:
+    -Update Info: Add description.
+    -Add icons.
+    -Update colors.
+
+###Others
+    -Set fonts color.
+    -Set principal colors.
+    -Set secondary colors.
+###Notes
+```
+:( u know what i mean
+http://reva.bouthythemes.com/home-2.html#home-cont
+```
--- src/index.js
+++ src/index.js
@@ -1,6 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+
+// Bool to verify if window.onload is called.
+let windowLoaded = false;
+
+// Bool to verify if the home image is loaded.
+let homeImageLoaded = false;
+
+// Create an instance of the home image.
+let homeImage = new Image();
+
+// Load the home image.
+homeImage.src = "IMG_20170716_191047.jpg";
+
+// Remove scrollbar while site is loading.
+document.getElementsByTagName("body")[0].style.overflow = "hidden";
+
+/**
+ * The onload callback is triggered when home image is loaded.
+ */
+homeImage.onload = () => {
+  homeImageLoaded = true;
+  preloaderEnd();
+};
+
+/**
+ * Set preloader end when site is loaded.
+ * Min time to set is 2 second.
+ */
+window.onload = () => {
+  windowLoaded = true;
+  preloaderEnd();
+};
+
+/**
+ * Hide preload when site is loaded.
+ * <br/>
+ * Home picture should be loaded.
+ */
+function preloaderEnd() {
+  if(homeImageLoaded && windowLoaded){
+    document.getElementsByTagName("body")[0].style.overflow = "inherit";
+    document.getElementById("preloader").classList.add("fadeOut");
+    setTimeout( () => {
+      document.getElementById("preloader").style.display = "none";
+    }, 1000);
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
